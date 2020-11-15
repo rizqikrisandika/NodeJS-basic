f2243922445f1b83bad0a055edcdba99d8ecd7c3
rizqikrisandika <rizqikrisandika990@gmail.com>
2020-11-15 14:15:32 +0700

    add tambah produk

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express()
@@ -20,16 +21,32 @@ connection.connect((error) => {
 app.set('views',path.join(__dirname,'views'));
 //set folder public static folder untuk static file
 app.use('/assets',express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 
 app.get('/',(req,res)=>{
-    let sql = "SELECT * FROM produk";
+    let sql = 'SELECT * FROM produk';
     let query =  connection.query(sql,(error, results)=>{
         res.render('produk.ejs',{produk: results});
     });   
 });
 
+app.get('/tambah_produk',(req,res)=>{
+    res.render('form_tambah.ejs');
+});
+
+app.post('/tambah',(req,res)=>{
+    let sql = 'INSERT INTO produk SET ?';
+    let data = {nama : req.body.nama,harga : req.body.harga};
+    let query = connection.query(sql, data,(error, results)=>{
+        res.redirect('/');
+    });
+});
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Aplikasi berjalan di port ${port}!`)
 });
